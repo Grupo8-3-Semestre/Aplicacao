4bcc281dfe80a475894b0d959e23d58312b38207
JoaoLeka <joao.barbosa@sptech.school>
2023-03-25 16:32:17 -0300

    adiciona respostas em http, utilizando ResponseEntity e status

--- login-logoff/src/main/java/school/sptech/loginlogoff/UsuarioController.java
+++ login-logoff/src/main/java/school/sptech/loginlogoff/UsuarioController.java
@@ -1,5 +1,6 @@
 package school.sptech.loginlogoff;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.text.ParseException;
@@ -13,15 +14,16 @@ public class UsuarioController {
     private List<Usuario> usuarioList = new ArrayList<>();
 
     @PostMapping
-    public UsuarioDTO cadastrar(@RequestBody Usuario novoUsuario) {
+    public ResponseEntity<UsuarioDTO> cadastrar(@RequestBody Usuario novoUsuario) {
         usuarioList.add(novoUsuario);
         UsuarioDTO userDTO = new UsuarioDTO(novoUsuario);
         usuarioDTOList.add(userDTO);
-        return userDTO;
+
+        return ResponseEntity.status(201).body(userDTO);
     }
 
     @PostMapping("/{email}/{senha}")
-    public UsuarioDTO autenticar(@PathVariable String email,
+    public ResponseEntity<UsuarioDTO> autenticar(@PathVariable String email,
                                  @PathVariable String senha) {
 
         for (int i =0; i < usuarioList.size(); i++) {
@@ -30,43 +32,60 @@ public class UsuarioController {
                 usuarioList.get(i).setAutenticado(true);
                 usuarioDTOList.get(i).setAutenticado(true);
 
-                return usuarioDTOList.get(i);
+                return ResponseEntity.status(202).body(usuarioDTOList.get(i));
             }
         }
-        return null;
+        return ResponseEntity.status(400).build();
+
     }
 
     @GetMapping
-    public List<UsuarioDTO> listar(){
-        return usuarioDTOList;
+    public ResponseEntity<List<UsuarioDTO>> listar(){
+        if(usuarioDTOList.isEmpty()){
+            return ResponseEntity.status(204).build();
+
+        }
+
+
+        return ResponseEntity.status(200).body(usuarioDTOList);
     }
 
     @DeleteMapping("/{email}")
-    public String logoff(@PathVariable String email){
+    public ResponseEntity<String>  logoff(@PathVariable String email){
 
         for(int i=0; i < usuarioList.size(); i++){
 
             if(usuarioList.get(i).getEmail().equals(email) && usuarioList.get(i).isAutenticado()){
                 usuarioList.get(i).setAutenticado(false);
                 usuarioDTOList.get(i).setAutenticado(false);
-                return String.format("Logoff do usuário %s concluído", usuarioList.get(i).getNomeUsuario());
+
+
+            return ResponseEntity.status(202).body(String.format("Logoff do usuário %s concluído", usuarioList.get(i).getNomeUsuario()));
+
             }
             else if(usuarioList.get(i).getEmail().equals(email) && !usuarioList.get(i).isAutenticado()){
-                return String.format("Usuário %s NÃO está autenticado", usuarioList.get(i).getNomeUsuario());
+
+                return ResponseEntity.status(400).body(String.format("Usuário %s NÃO está autenticado", usuarioList.get(i).getNomeUsuario()));
+
             }
         }
-        return String.format("Usuário %s não encontrado", email);
+        return ResponseEntity.status(404).body(String.format("Usuário %s não encontrado", email));
     }
 
     @GetMapping("/nomes")
-    public List<String> mostrarNomes(){
+    public ResponseEntity<List<String>> mostrarNomes(){
 
         List<String> nomes = new ArrayList<>();
 
         for (Usuario u : usuarioList){
             nomes.add(u.getNomeUsuario());
         }
-        return nomes;
+        if(nomes.isEmpty()){
+            return ResponseEntity.status(204).build();
+
+        }
+
+        return ResponseEntity.status(200).body(nomes);
     }
 
 }
